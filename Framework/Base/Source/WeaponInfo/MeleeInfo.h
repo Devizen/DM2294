bfdcd55c060d550718bb9fcac713a86a80134c21
Yu Ming <greatreaperzsnip3@gmail.com>
2017-08-17 10:24:57 +0800

    Updated Melee class

--- Framework/Base/Source/WeaponInfo/MeleeInfo.h
+++ Framework/Base/Source/WeaponInfo/MeleeInfo.h
@@ -11,15 +11,6 @@ public:
 	CMeleeInfo();
 	virtual ~CMeleeInfo();
 protected:
-	//// The number of ammunition in a magazine for this weapon
-	//int magRounds;
-	//// The maximum number of ammunition for this magazine for this weapon
-	//int maxMagRounds;
-	//// The current total number of rounds currently carried by this player
-	//int totalRounds;
-	//// The max total number of rounds currently carried by this player
-	//int maxTotalRounds;
-
 	// The time between shots in milliseconds
 	double timeBetweenShots;
 	// The elapsed time (between shots) in milliseconds
@@ -27,28 +18,7 @@ protected:
 	// Boolean flag to indicate if weapon can fire now
 	bool bFire;
 
-	////Default maxTotalRounds
-	//int defaultMaxTotalRounds;
-
 public:
-	//// Set the number of ammunition in the magazine for this player
-	//virtual void SetMagRound(const int magRounds);
-	//// Set the maximum number of ammunition in the magazine for this weapon
-	//virtual void SetMaxMagRound(const int magRounds);
-	//// The current total number of rounds currently carried by this player
-	//virtual void SetTotalRound(const int totalRounds);
-	//// The max total number of rounds currently carried by this player
-	//virtual void SetMaxTotalRound(const int maxTotalRounds);
-
-	//// Get the number of ammunition in the magazine for this player
-	//virtual int GetMagRound(void) const;
-	//// Get the maximum number of ammunition in the magazine for this weapon
-	//virtual int GetMaxMagRound(void) const;
-	//// Get the current total number of rounds currently carried by this player
-	//virtual int GetTotalRound(void) const;
-	//// Get the max total number of rounds currently carried by this player
-	//virtual int GetMaxTotalRound(void) const;
-
 	// Set the time between shots
 	virtual void SetTimeBetweenShots(const double timeBetweenShots);
 	// Set the firing rate in rounds per min
@@ -69,14 +39,4 @@ public:
 	void Update(const double dt);
 	// Discharge this weapon
 	void Slash(Vector3 position, Vector3 target, CPlayerInfo* _source = NULL);
-	//// Reload this weapon
-	//void Reload(void);
-	//// Add rounds
-	//void AddRounds(const int newRounds);
-
-	//// Print Self
-	//void PrintSelf(void);
-
-	////Get default max total rounds
-	//int getDefaultMaxTotalRounds(void);
 };
